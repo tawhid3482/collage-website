cc3dca91e4d8656e698afe7e753990ad692c4ede
saikat <tawhidulislam3482@gmail.com>
2023-12-20 11:35:34 +0600

    error page complete

--- src/Components/Navber/Navber.jsx
+++ src/Components/Navber/Navber.jsx
@@ -10,13 +10,7 @@ const Navber = () => {
       </div>
       <ul tabIndex={0} className="menu menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
         <li><a>Item 1</a></li>
-        <li>
-          <a>Parent</a>
-          <ul className="p-2">
-            <li><a>Submenu 1</a></li>
-            <li><a>Submenu 2</a></li>
-          </ul>
-        </li>
+       
         <li><a>Item 3</a></li>
       </ul>
     </div>
