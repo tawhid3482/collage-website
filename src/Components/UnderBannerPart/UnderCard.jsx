17ef40ab16763c01b23913d30b1881df23a589fd
saikat <tawhidulislam3482@gmail.com>
2023-12-21 20:24:16 +0600

    complete

--- src/Components/UnderBannerPart/UnderCard.jsx
+++ src/Components/UnderBannerPart/UnderCard.jsx
@@ -4,11 +4,11 @@ const UnderCard = ({ under }) => {
   return (
     <div>
       
-      <div className="w-72 h-52 bg-green-800 rounded-md">
+      <div className="w-72 h-52  rounded-md">
         <div className="absolute">
-            <img src={img} className="w-72 h-52 " alt="" />
+            <img src={img} className="w-72 h-52 opacity-35 " alt="" />
         </div>
-        <div className="relative text-white top-20 text-center">
+        <div className="relative text-green-700 top-20 text-center">
             <p className="text-3xl  font-bold">{name}</p>
             <p>{title}</p>
         </div>
