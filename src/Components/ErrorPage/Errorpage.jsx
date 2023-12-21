d491686c58240f2649a0f3a45e1cb24cecc4815c
saikat <tawhidulislam3482@gmail.com>
2023-12-21 21:07:29 +0600

    about part complete

--- src/Components/ErrorPage/Errorpage.jsx
+++ src/Components/ErrorPage/Errorpage.jsx
@@ -6,7 +6,7 @@ const Errorpage = () => {
       <img src={error} className="w-full h-screen absolute" alt="" />
       <Link to="/">
         
-        <button className="btn bg-green-800  relative left-32 top-[335px] md:left-96 md:top-[335px] lg:top-[450px] lg:left-[1060px] text-white">GO HOME</button>
+        <button className="btn bg-green-800 relative left-32 top-[335px] md:left-96 md:top-[335px] lg:top-[450px] lg:left-[1060px] text-white">GO HOME</button>
       </Link>
     </div>
   );
