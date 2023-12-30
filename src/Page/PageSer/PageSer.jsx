1d43112514e45d04af9f188b064f2f8acd5e85ce
saikat <tawhidulislam3482@gmail.com>
2023-12-30 20:26:06 +0600

    error fixed

--- src/Page/PageSer/PageSer.jsx
+++ src/Page/PageSer/PageSer.jsx
@@ -2,22 +2,25 @@ import UsePage from "../../Hooks/UsePage";
 import PageService from "./PageService";
 
 const PageSer = () => {
-    const [services]=UsePage()
+  const [services] = UsePage();
   return (
     <div>
       <div
-        className="hero h-[400px] "
+        className="hero ld:h-[400px] "
         style={{
           backgroundImage:
             "url(https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcT2r2XiSYedi-DYysldZzD8qU9CDbHG5zPiCxlNOy1VtZ2zJODs0EzJy9PI1SgZiNRNMqU&usqp=CAU)",
         }}
       >
         <div className="hero-overlay bg-opacity-60"></div>
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
-                {
-                    services?.map((service)=><PageService key={service.title} service={service}> </PageService>)
-                }
-            </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5">
+          {services?.map((service) => (
+            <PageService key={service.title} service={service}>
+              {" "}
+            </PageService>
+          ))}
+        </div>
       </div>
     </div>
   );
--- src/Page/PageSer/PageService.jsx
+++ src/Page/PageSer/PageService.jsx
@@ -1,18 +1,17 @@
-
-const PageService = ({service}) => {
-    // console.log(service)
-    const {img,title,name}=service
-    return (
-        <div>
-            <div className="text-white">
-                <div className="flex border p-3 border-green-600 justify-center">
-                <img src={img} className="w-80 h-40" alt="" />
-                </div>
-                <p className="text-2xl font-bold my-5">{name}</p>
-                <p>{title}</p>
-            </div>
+const PageService = ({ service }) => {
+  // console.log(service)
+  const { img, title, name } = service;
+  return (
+    <div className="p-4">
+      <div className="text-white ">
+        <div className="flex border p-3 border-green-600 justify-center">
+          <img src={img} className="w-80 h-40" alt="" />
         </div>
-    );
+        <p className="text-2xl font-bold my-5">{name}</p>
+        <p>{title}</p>
+      </div>
+    </div>
+  );
 };
 
 export default PageService;
