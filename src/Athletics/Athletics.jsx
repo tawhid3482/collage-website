0e199cdb760d1ca5116735fc479d496f73b8dea4
saikat <tawhidulislam3482@gmail.com>
2024-01-02 19:41:06 +0600

    image zoom in complate

--- src/Athletics/Athletics.jsx
+++ src/Athletics/Athletics.jsx
@@ -38,20 +38,85 @@ const Athletics = () => {
 
         <span className="text-4xl font-bold">Upcoming Events</span>
         <hr className="border border-gray-400 w-32" />
-
       </div>
       <p className="text-lg text-green-500 ml-1">Read All News</p>
-      <div className="flex justify-between items-start my-8 w-1/3">
-        <div className="group">
-            <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTavZYhYHiG6xw81kf49kan9xK0qALB7UaLRA&usqp=CAU" alt="" className="md:w-96 duration-300 group-hover:scale-110" />
-        <p className=" text-slate-500 my-2 mt-8" >JUNE 6, / 2016ADMISSION, STUDENT</p>
-        <p className="text-2xl font-bold hover:text-green-500" >Professor Albert joint research on mobile money in Tanzania</p>
+      <div className="flex justify-between items-start gap-7 my-8 ">
+        <div className=" relative overflow-hidden group lg:w-1/3">
+          <img
+            src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTavZYhYHiG6xw81kf49kan9xK0qALB7UaLRA&usqp=CAU"
+            alt=""
+            className="md:w-96 duration-300 group-hover:scale-110"
+          />
+          <p className=" text-slate-500 my-2 mt-8">
+            JUNE 6, / 2016ADMISSION, STUDENT
+          </p>
+          <p className="text-2xl font-bold hover:text-green-500">
+            Professor Albert joint research on mobile money in Tanzania
+          </p>
         </div>
-        <div className="">
 
+        <div className="lg:w-1/3 ">
+          <div className="flex gap-5  relative overflow-hidden group">
+            <img
+              src="https://www.ringcentral.com/gb/en/blog/wp-content/uploads/2021/05/happy-manager-leads-a-meeting-in-the-office-scaled.jpg"
+              className="w-28 duration-300 group-hover:scale-110 "
+              alt=""
+            />
+            <div className="">
+              <p>JUNE 6, / 2016HOT, UPDATES</p>
+              <p className="text-lg font-bold hover:text-green-500">
+                A Global MBA for the next generation of business leaders
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-5 my-5 relative overflow-hidden group">
+            <img
+              src="https://www.ringcentral.com/gb/en/blog/wp-content/uploads/2021/05/happy-manager-leads-a-meeting-in-the-office-scaled.jpg"
+              className="w-28 duration-300 group-hover:scale-110 "
+              alt=""
+            />
+            <div className="">
+              <p>JUNE 6, / 2016HOT, UPDATES</p>
+              <p className="text-lg font-bold hover:text-green-500">
+                A Global MBA for the next generation of business leaders
+              </p>
+            </div>
+          </div>
+          <div className="flex gap-5  relative overflow-hidden group">
+            <img
+              src="https://www.ringcentral.com/gb/en/blog/wp-content/uploads/2021/05/happy-manager-leads-a-meeting-in-the-office-scaled.jpg"
+              className="w-28 duration-300 group-hover:scale-110 "
+              alt=""
+            />
+            <div className="">
+              <p>JUNE 6, / 2016HOT, UPDATES</p>
+              <p className="text-lg font-bold hover:text-green-500">
+                A Global MBA for the next generation of business leaders
+              </p>
+            </div>
+          </div>
         </div>
-        <div className=""></div>
 
+        <div className="lg:w-1/3 border border-r-emerald-400 p-1">
+          <div className=" flex items-center gap-5">
+           <div className="">
+           <p className="text-2xl text-green-500 font-bold">17 <br /> DEC</p>
+            <hr className="border-2 border-green-500 w-12" />
+           </div>
+           <div className="">
+           <p className=" text-2xl font-bold hover:text-green-500 ">
+           Fintech & Key Investment Conference
+
+            </p>
+            <p className="">
+              Professor Albert joint research on mobile money in Tanzania
+            </p>
+            <div className="">
+                
+            </div>
+           </div>
+          </div>
+        </div>
       </div>
     </div>
   );
