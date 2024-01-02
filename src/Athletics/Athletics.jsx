52c6e1ffee8ee667661b0be29e4d71b499dff84f
saikat <tawhidulislam3482@gmail.com>
2024-01-02 18:34:17 +0600

    athletics is working

--- src/Athletics/Athletics.jsx
+++ src/Athletics/Athletics.jsx
@@ -16,14 +16,29 @@ const Athletics = () => {
         <hr className="border-2 border-green-500 w-80" />
       </div>
       <div className="">
-        <p className="text-lg text-green-500 mt-5" >
+        <p className="text-lg text-green-500 mt-5">
           Far far away, behind the word mountains, far from the countries
-          Vokalia and Consonantia, <br /> there live the blind texts. Separated they
-          live in
+          Vokalia and Consonantia, <br /> there live the blind texts. Separated
+          they live in
         </p>
         <p className="text-lg mt-2">
-        Bookmarksgrove right at the coast of the Semantics, a large language ocean. A small river named Duden flows by their place and supplies it with the necessary regelialia. It is a paradisematic country, in which roasted parts of sentences fly into your mouth. Even the all-powerful Pointing has no control about the blind texts it is an almost unorthographic life One day however a small line of blind text by the name of Lorem Ipsum decided to leave for the far World of Grammar. The Big Oxmox advised her not to do so.
+          Bookmarksgrove right at the coast of the Semantics, a large language
+          ocean. A small river named Duden flows by their place and supplies it
+          with the necessary regelialia. It is a paradisematic country, in which
+          roasted parts of sentences fly into your mouth. Even the all-powerful
+          Pointing has no control about the blind texts it is an almost
+          unorthographic life One day however a small line of blind text by the
+          name of Lorem Ipsum decided to leave for the far World of Grammar. The
+          Big Oxmox advised her not to do so.
         </p>
+      </div>
+      <div className="mt-10 flex justify-center gap-2 items-center">
+        <span className="text-4xl font-bold">News & Updates</span>
+        <hr className="border border-gray-400 w-[475px]" />
+
+        <span className="text-4xl font-bold">Upcoming Events</span>
+        <hr className="border border-gray-400 w-32" />
+
       </div>
     </div>
   );
