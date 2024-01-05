9adee6e6276a305f435dafe0509560844dbe3734
saikat <tawhidulislam3482@gmail.com>
2024-01-05 19:52:16 +0600

    customize

--- src/Components/UnderBannerPart/UnderCard.jsx
+++ src/Components/UnderBannerPart/UnderCard.jsx
@@ -3,11 +3,11 @@ const UnderCard = ({ under }) => {
   return (
     <div>
       
-      <div className="w-72 h-52  rounded-md">
+      <div className="hero-overlay w-72 h-52  rounded-md">
         <div className="absolute">
-            <img src={img} className="w-72 h-52 opacity-35 " alt="" />
+            <img src={img} className="w-72 h-52 opacity-60 " alt="" />
         </div>
-        <div className="relative text-green-700 top-20 text-center">
+        <div className="relative text-white top-20 text-center">
             <p className="text-3xl  font-bold">{name}</p>
             <p>{title}</p>
         </div>
