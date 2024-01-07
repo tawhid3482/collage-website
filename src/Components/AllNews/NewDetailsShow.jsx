9857b502fc1a4b1edcdf256f278cad5c4aaebb7b
saikat <tawhidulislam3482@gmail.com>
2024-01-07 18:11:32 +0600

    news details woriking

--- src/Components/AllNews/NewDetailsShow.jsx
+++ src/Components/AllNews/NewDetailsShow.jsx
@@ -1,11 +1,23 @@
-
-const NewDetailsShow = ({newS}) => {
-    const {name,title,date,img} = newS
-    return (
-        <div>
-            
+const NewDetailsShow = ({ newS }) => {
+  const { name, title, date, img } = newS;
+  return (
+    <div>
+      <div className="hero min-h-screen bg-base-200">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <img
+            src={img}
+            className="w-1/2 rounded-lg shadow-2xl"
+          />
+          <div>
+            <h1 className="text-5xl font-bold">{name}</h1>
+            <p className="py-6">
+            {title}
+            </p>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default NewDetailsShow;
