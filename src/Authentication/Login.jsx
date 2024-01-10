49e74ea06d8ec484d3acae3ce3e5b04f891f3181
saikat <tawhidulislam3482@gmail.com>
2024-01-10 11:29:50 +0600

    login form woriki g|

--- src/Authentication/Login.jsx
+++ src/Authentication/Login.jsx
@@ -1,6 +1,25 @@
+import { useEffect } from "react";
 import Helmets from "../Helmets/Helmets";
-import security from '../assets/global-data-security-personal-data-security-cyber-data-security-online-concept-illustration-internet-security-information-privacy-protection_1150-37373.avif'
+import security from "../assets/global-data-security-personal-data-security-cyber-data-security-online-concept-illustration-internet-security-information-privacy-protection_1150-37373.avif";
+import {
+  loadCaptchaEnginge,
+  LoadCanvasTemplate,
+  LoadCanvasTemplateNoReload,
+  validateCaptcha,
+} from "react-simple-captcha";
+
 const Login = () => {
+  useEffect(() => {
+    loadCaptchaEnginge(6);
+  }, []);
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    console.log(email, password);
+  };
   return (
     <div>
       <div className="">
@@ -12,13 +31,14 @@ const Login = () => {
             <img src={security} className="w-full" alt="" />
           </div>
           <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-            <form className="card-body h-full">
+            <form onSubmit={handleSubmit} className="card-body h-full">
               <div className="form-control">
                 <label className="label">
                   <span className="label-text">Email</span>
                 </label>
                 <input
                   type="email"
+                  name="email"
                   placeholder="email"
                   className="input input-bordered"
                   required
@@ -31,17 +51,29 @@ const Login = () => {
                 <input
                   type="password"
                   placeholder="password"
+                  name="password"
                   className="input input-bordered"
                   required
                 />
+              </div>
+              <div className="form-control">
                 <label className="label">
-                  <a href="#" className="label-text-alt link link-hover">
-                    Forgot password?
-                  </a>
+                  <LoadCanvasTemplate />
                 </label>
+                <input
+                  type="text"
+                  placeholder="Captcha"
+                  name="captcha"
+                  className="input input-bordered"
+                  required
+                />
               </div>
               <div className="form-control mt-6">
-                <button className="btn text-transparent bg-gradient-to-r from-purple-600 to-pink-600 text-white">Login</button>
+                <input
+                  type="submit"
+                  className="btn text-transparent bg-gradient-to-r from-purple-600 to-pink-600 text-white"
+                  value="Login"
+                />
               </div>
             </form>
           </div>
