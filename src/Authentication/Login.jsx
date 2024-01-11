8bf949262b0e9a1346a0c4d0aaf66b6ce1e21af4
saikat <tawhidulislam3482@gmail.com>
2024-01-11 08:56:54 +0600

    reCaptacha done

--- src/Authentication/Login.jsx
+++ src/Authentication/Login.jsx
@@ -1,14 +1,15 @@
-import { useEffect } from "react";
+import { useEffect, useRef, useState } from "react";
 import Helmets from "../Helmets/Helmets";
 import security from "../assets/global-data-security-personal-data-security-cyber-data-security-online-concept-illustration-internet-security-information-privacy-protection_1150-37373.avif";
 import {
   loadCaptchaEnginge,
   LoadCanvasTemplate,
-  LoadCanvasTemplateNoReload,
   validateCaptcha,
 } from "react-simple-captcha";
 
 const Login = () => {
+  const captchaRef = useRef(null);
+  const [disabled, setDisabled] = useState(true);
   useEffect(() => {
     loadCaptchaEnginge(6);
   }, []);
@@ -19,6 +20,15 @@ const Login = () => {
     const email = form.email.value;
     const password = form.password.value;
     console.log(email, password);
+  };
+  const handleValiedCaptcha = () => {
+    const user_captcha_value = captchaRef.current.value;
+    // console.log(value);
+    if (validateCaptcha(user_captcha_value)) {
+      setDisabled(false);
+    } else {
+      setDisabled(true);
+    }
   };
   return (
     <div>
@@ -64,15 +74,23 @@ const Login = () => {
                   type="text"
                   placeholder="Captcha"
                   name="captcha"
+                  ref={captchaRef}
                   className="input input-bordered"
                   required
                 />
+                <button
+                  onClick={handleValiedCaptcha}
+                  className="btn btn-outline btn-xs mt-2 btn-error"
+                >
+                  Error
+                </button>
               </div>
               <div className="form-control mt-6">
                 <input
                   type="submit"
-                  className="btn text-transparent bg-gradient-to-r from-purple-600 to-pink-600 text-white"
+                  className="btn text-transparent bg-gradient-to-r from-purple-600 to-pink-600 text-white hover:text-black hover:bg-gray-600"
                   value="Login"
+                  disabled={disabled}
                 />
               </div>
             </form>
