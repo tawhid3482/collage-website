f61a41a29cebfa28d0346c7ad50c2e4feba40bae
saikat <tawhidulislam3482@gmail.com>
2024-01-17 10:25:45 +0600

    dashboard working

--- src/Components/LayOut/Dashboard.jsx
+++ src/Components/LayOut/Dashboard.jsx
@@ -2,25 +2,24 @@ import { FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
 
 const Dashboard = () => {
-    return (
-        <div>
-            <div className="flex"> 
-            
-            <div className="w-64 min-h-full bg-gradient-to-t from-purple-600 to-pink-600">
-                <ul className="menu">
-                    <li className="">
-                        <NavLink to='/dashboard/cart'>My Cart <FaShoppingCart className="text-xl "></FaShoppingCart></NavLink>
-                        
-                        </li>
-                </ul>
-            </div>
-            <div className="flex-1">
-                <Outlet></Outlet>
-            </div>
-            
-            </div>
+  return (
+    <div>
+      <div className="flex">
+        <div className="w-64 min-h-full bg-gradient-to-t from-purple-600 to-pink-600">
+          <ul className="menu p-4">
+            <li className="">
+              <NavLink to="/dashboard/cart">
+                My Cart <FaShoppingCart className="text-xl text-purple-600"></FaShoppingCart>
+              </NavLink>
+            </li>
+          </ul>
         </div>
-    );
+        <div className="flex-1">
+          <Outlet></Outlet>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Dashboard;
