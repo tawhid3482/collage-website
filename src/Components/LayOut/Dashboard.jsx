66a0628190384d194cabbbd7c27fc5986aadd3e2
saikat <tawhidulislam3482@gmail.com>
2024-01-17 10:27:52 +0600

    user home

--- src/Components/LayOut/Dashboard.jsx
+++ src/Components/LayOut/Dashboard.jsx
@@ -8,6 +8,11 @@ const Dashboard = () => {
         <div className="w-64 min-h-full bg-gradient-to-t from-purple-600 to-pink-600">
           <ul className="menu p-4">
             <li className="">
+              <NavLink to="/dashboard/userHome">
+                User Home <FaShoppingCart className="text-xl text-purple-600"></FaShoppingCart>
+              </NavLink>
+            </li>
+            <li className="">
               <NavLink to="/dashboard/cart">
                 My Cart <FaShoppingCart className="text-xl text-purple-600"></FaShoppingCart>
               </NavLink>
