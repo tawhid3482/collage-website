2a1e90dcdf580387e8db3dc61da41325c75055f8
saikat <tawhidulislam3482@gmail.com>
2024-01-21 11:16:56 +0600

    cart working

--- src/Components/Dashboard/MYCart/MyCart.jsx
+++ src/Components/Dashboard/MYCart/MyCart.jsx
@@ -1,8 +1,16 @@
+import UseCart from "../../../Hooks/UseCart";
+import Button from "../../Shayed/Button";
 
 const MyCart = () => {
+  const [cart]=UseCart()
+  console.log(cart)
     return (
         <div>
-          cart
+         <div className="flex justify-between items-center my-4">
+          <p className="text-4xl font-bold">Total Course: {cart?.length}</p>
+          <p className="text-4xl font-bold">Total Fee:{cart?.fee}</p>
+          <Button text={'Pay'}></Button>
+         </div>
         </div>
     );
 };
--- src/Components/LayOut/Dashboard.jsx
+++ src/Components/LayOut/Dashboard.jsx
@@ -6,25 +6,52 @@ const Dashboard = () => {
     <div>
       <div className="flex gap-5">
         <div className="w-64 min-h-full bg-gradient-to-t from-purple-600 to-pink-600">
-          <ul className="menu p-4">
-            <li className="text-yellow-400 uppercase">
+          <ul className="menu p-5">
+            <li className="text-white mb-2 uppercase">
               <NavLink to="/dashboard/userHome">
-                User Home <FaHome className="text-xl ml-5"></FaHome>
+              <FaHome className="text-xl mr-2"></FaHome>  Student Home 
               </NavLink>
             </li>
-            <li className="text-yellow-400 uppercase">
+            <li className="text-white mb-2 uppercase">
               <NavLink to="/dashboard/myCourse">
-                My Course <FaBook className="text-xl ml-5 "></FaBook>
+              <FaBook className="text-xl mr-2 "></FaBook>  My Course 
               </NavLink>
             </li>
-            <li className="text-yellow-400 uppercase">
+            <li className="text-white mb-2 uppercase">
               <NavLink to="/dashboard/cart">
-                My Cart <FaShoppingCart className="text-xl ml-5 "></FaShoppingCart>
+              <FaShoppingCart className="text-xl mr-2 "></FaShoppingCart> My Cart 
               </NavLink>
             </li>
-            <li className="text-yellow-400 uppercase">
+            <li className="text-white mb-2 uppercase">
               <NavLink to="/dashboard/history">
-                My History <FaHistory className="text-xl ml-5 "></FaHistory>
+              <FaHistory className="text-xl mr-2 "></FaHistory>  My History 
+              </NavLink>
+            </li>
+            <div className="divider text-white">OR</div>
+            <li className="text-white mb-2 uppercase">
+              <NavLink to="/">
+                 Home 
+              </NavLink>
+            </li>
+           
+            <li className="text-white mb-2 uppercase">
+              <NavLink to="/page">
+                 Page 
+              </NavLink>
+            </li>
+            <li className="text-white mb-2 uppercase">
+              <NavLink to="/course">
+                 course 
+              </NavLink>
+            </li>
+            <li className="text-white mb-2 uppercase">
+              <NavLink to="/athletics">
+                 Athletics 
+              </NavLink>
+            </li>
+            <li className="text-white mb-2 uppercase">
+              <NavLink to="/university">
+                 University Life 
               </NavLink>
             </li>
           </ul>
