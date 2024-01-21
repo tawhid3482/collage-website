2f3c03b26fac16e1058a129c9d13f859bff91466
saikat <tawhidulislam3482@gmail.com>
2024-01-21 11:59:08 +0600

    cart working

--- src/Components/Dashboard/MYCart/MyCart.jsx
+++ src/Components/Dashboard/MYCart/MyCart.jsx
@@ -1,3 +1,4 @@
+import { FaTrash } from "react-icons/fa";
 import UseCart from "../../../Hooks/UseCart";
 import Button from "../../Shayed/Button";
 
@@ -12,6 +13,32 @@ const MyCart = () => {
           <p className="text-4xl font-bold">Total Fee: {totalPrice}</p>
           <Button text={'Pay'}></Button>
          </div>
+
+         <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr className="text-xl font-semibold">
+        <th >No.</th>
+        <th>Course</th>
+        <th>Fee</th>
+        <th>Action</th>
+      </tr>
+    </thead>
+    <tbody>
+      {
+        cart?.map((item)=><tr key={item?._id}>
+        <th></th>
+        <td className="text-lg">{item?.name}</td>
+        <td className="text-lg">{item?.fee}</td>
+        <td> <button className="btn"><FaTrash className="text-xl text-pink-600"></FaTrash></button> </td>
+      </tr>)
+      }
+      
+     
+    </tbody>
+  </table>
+</div>
         </div>
     );
 };
