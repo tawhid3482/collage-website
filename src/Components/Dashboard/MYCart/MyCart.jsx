c18058b594722e9f01d66fa118df2a7f64d6d0b8
saikat <tawhidulislam3482@gmail.com>
2024-01-21 11:45:54 +0600

    error fix

--- src/Components/Dashboard/MYCart/MyCart.jsx
+++ src/Components/Dashboard/MYCart/MyCart.jsx
@@ -3,12 +3,13 @@ import Button from "../../Shayed/Button";
 
 const MyCart = () => {
   const [cart]=UseCart()
-  console.log(cart)
-    return (
+  // console.log(cart)
+  const totalPrice = cart.reduce((total, item) => total + item.fee, 0)
+  return (
         <div>
          <div className="flex justify-between items-center my-4">
           <p className="text-4xl font-bold">Total Course: {cart?.length}</p>
-          <p className="text-4xl font-bold">Total Fee:{cart?.fee}</p>
+          <p className="text-4xl font-bold">Total Fee: {totalPrice}</p>
           <Button text={'Pay'}></Button>
          </div>
         </div>
