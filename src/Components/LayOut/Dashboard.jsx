8bb20fe166cceef65565a6098db187f73e2988cf
saikat <tawhidulislam3482@gmail.com>
2024-01-21 18:02:24 +0600

    dashboard working

--- src/Components/LayOut/Dashboard.jsx
+++ src/Components/LayOut/Dashboard.jsx
@@ -1,11 +1,15 @@
-import { FaBook, FaHistory, FaHome, FaShoppingCart } from "react-icons/fa";
+import { FaBook, FaHistory, FaHome,  FaLifeRing,  FaPager, FaShoppingCart } from "react-icons/fa";
 import { NavLink, Outlet } from "react-router-dom";
+import { SiCoursera } from "react-icons/si";
+import { MdSportsMartialArts } from "react-icons/md";
+import { GiLoveMystery } from "react-icons/gi";
+
 
 const Dashboard = () => {
   return (
     <div>
       <div className="flex gap-5">
-        <div className="w-64 min-h-full bg-gradient-to-t from-purple-600 to-pink-600">
+        <div className="w-64  min-h-full h-screen bg-gradient-to-t from-purple-600 to-pink-600">
           <ul className="menu p-5">
             <li className="text-white mb-2 uppercase">
               <NavLink to="/dashboard/userHome">
@@ -30,28 +34,28 @@ const Dashboard = () => {
             <div className="divider text-white">OR</div>
             <li className="text-white mb-2 uppercase">
               <NavLink to="/">
-                 Home 
+              <FaHome className="text-xl mr-2 "></FaHome>   Home 
               </NavLink>
             </li>
            
             <li className="text-white mb-2 uppercase">
               <NavLink to="/page">
-                 Page 
+              <FaPager className="text-xl mr-2 "></FaPager>  Page 
               </NavLink>
             </li>
             <li className="text-white mb-2 uppercase">
               <NavLink to="/course">
-                 course 
+              <SiCoursera className="text-xl mr-2 "></SiCoursera> course 
               </NavLink>
             </li>
             <li className="text-white mb-2 uppercase">
               <NavLink to="/athletics">
-                 Athletics 
+              <MdSportsMartialArts className="text-xl mr-2 "></MdSportsMartialArts>  Athletics 
               </NavLink>
             </li>
             <li className="text-white mb-2 uppercase">
               <NavLink to="/university">
-                 University Life 
+              <GiLoveMystery className="text-xl mr-2 "></GiLoveMystery>  University Life 
               </NavLink>
             </li>
           </ul>
