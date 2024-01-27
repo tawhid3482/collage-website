4ae3d66b3db9c7611ea18f6ad1cb6c1f3cbc90a2
saikat <tawhidulislam3482@gmail.com>
2024-01-27 10:12:38 +0600

    i am sorry

--- src/Components/LayOut/Dashboard.jsx
+++ src/Components/LayOut/Dashboard.jsx
@@ -11,43 +11,59 @@ import { SiCoursera } from "react-icons/si";
 import { MdSportsMartialArts } from "react-icons/md";
 import { GiLoveMystery } from "react-icons/gi";
 import UseCart from "../../Hooks/UseCart";
+import { MdOutlineManageSearch } from "react-icons/md";
+
+// i am sorry i am seek
 
 const Dashboard = () => {
   const isAdmin = true;
-  const isModerator = true;
+  const isModerator = false;
   const [cart] = UseCart();
+
   return (
     <div>
       <div className="md:flex md:gap-5">
         <div className="md:w-64 h-full md:h-screen bg-gradient-to-t from-purple-600 to-pink-600">
           <ul className="menu p-5">
-            {/* for user  */}
-            {isAdmin || isModerator ? (
+            {isAdmin && (
               <>
-              <li className="text-white mb-2 uppercase">
+                <li className="text-white mb-2 uppercase">
                   <NavLink to="/dashboard/AdminHome">
                     <FaHome className="text-xl mr-2"></FaHome> Admin Home
                   </NavLink>
                 </li>
-              <li className="text-white mb-2 uppercase">
+                <li className="text-white mb-2 uppercase">
                   <NavLink to="/dashboard/addCourse">
                     <FaBook className="text-xl mr-2"></FaBook> Add course
                   </NavLink>
                 </li>
-              <li className="text-white mb-2 uppercase">
+                <li className="text-white mb-2 uppercase">
                   <NavLink to="/dashboard/manageCourse">
-                    <FaHome className="text-xl mr-2"></FaHome> Manage course
+                    <MdOutlineManageSearch className="text-2xl mr-2"></MdOutlineManageSearch>{" "}
+                    Manage course
                   </NavLink>
                 </li>
-              <li className="text-white mb-2 uppercase">
+                <li className="text-white mb-2 uppercase">
                   <NavLink to="/dashboard/allUser">
-                    <FaUsers className="text-xl mr-2"></FaUsers> all users
+                    <FaUsers className="text-xl mr-2"></FaUsers> All Students
                   </NavLink>
                 </li>
-              
               </>
-            ) : (
+            )}
+
+            {isModerator && (
               <>
+                <li className="text-white mb-2 uppercase">
+                  <NavLink to="/dashboard/allUser">
+                    <FaUsers className="text-xl mr-2"></FaUsers> All Students
+                  </NavLink>
+                </li>
+              </>
+            )}
+
+            {!isAdmin && !isModerator && (
+              <>
+                {/* Panel for User */}
                 <li className="text-white mb-2 uppercase">
                   <NavLink to="/dashboard/studentHome">
                     <FaHome className="text-xl mr-2"></FaHome> Student Home
@@ -77,13 +93,12 @@ const Dashboard = () => {
 
             <div className="divider text-white">OR</div>
 
-            {/* for visitor */}
+            {/* Common navigation items for all roles */}
             <li className="text-white mb-2 uppercase">
               <NavLink to="/">
                 <FaHome className="text-xl mr-2 "></FaHome> Home
               </NavLink>
             </li>
-
             <li className="text-white mb-2 uppercase">
               <NavLink to="/page">
                 <FaPager className="text-xl mr-2 "></FaPager> Page
@@ -91,7 +106,7 @@ const Dashboard = () => {
             </li>
             <li className="text-white mb-2 uppercase">
               <NavLink to="/course">
-                <SiCoursera className="text-xl mr-2 "></SiCoursera> course
+                <SiCoursera className="text-xl mr-2 "></SiCoursera> Course
               </NavLink>
             </li>
             <li className="text-white mb-2 uppercase">
