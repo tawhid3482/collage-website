93390f996cbb7d6a37331469344d8ee2f0e6784c
saikat <tawhidulislam3482@gmail.com>
2024-02-07 18:47:50 +0600

    jwt verify fix

--- src/Hooks/UseAxiosSecure.jsx
+++ src/Hooks/UseAxiosSecure.jsx
@@ -1,12 +1,20 @@
 import axios from "axios";
 
- const axiosSecure = axios.create({
-    baseURL:'http://localhost:5000',
-
-})
+const axiosSecure = axios.create({
+  baseURL: "http://localhost:5000",
+});
 const UseAxiosSecure = () => {
-   return axiosSecure
-
+  axiosSecure.interceptors.request.use(
+    function (config) {
+      // Do something before request is sent
+      return config;
+    },
+    function (error) {
+      // Do something with request error
+      return Promise.reject(error);
+    }
+  );
+  return axiosSecure;
 };
 
 export default UseAxiosSecure;
