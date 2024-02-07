e75852ad838fdeed93aae7ffbcc5855ba8e5a56e
saikat <tawhidulislam3482@gmail.com>
2024-02-07 15:44:24 +0600

    jwt errror fix

--- src/Components/Dashboard/AllStudents/AllStudents.jsx
+++ src/Components/Dashboard/AllStudents/AllStudents.jsx
@@ -8,7 +8,11 @@ const AllStudents = () => {
   const { data, refetch } = useQuery({
     queryKey: ["users"],
     queryFn: async () => {
-      const res = await axiosSecure.get("/users");
+      const res = await axiosSecure.get("/users", {
+        headers: {
+          authorization: `Bearer ${localStorage.getItem("access-token")}`,
+        },
+      });
       return res.data;
     },
   });
