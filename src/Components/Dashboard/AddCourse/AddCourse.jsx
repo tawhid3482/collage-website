52eb8e091d8e0bc8b1cc16cf942c6b4f6629db9c
saikat <tawhidulislam3482@gmail.com>
2024-02-10 18:46:51 +0600

    add course from working

--- src/Components/Dashboard/AddCourse/AddCourse.jsx
+++ src/Components/Dashboard/AddCourse/AddCourse.jsx
@@ -11,13 +11,198 @@ const AddCourse = () => {
       </div>
       <div className="">
         <form onSubmit={handleSubmit(onSubmit)}>
-          <label>Subject Name</label>
-          <input {...register("SubjectName")} />
-          <select className="select select-primary w-full max-w-xs" {...register("Semester")}>
-            <option disabled selected value="1st">1st</option>
-            <option value="3rd">3rd</option>
-            <option value="5th">5th</option>
-          </select>
+          <label className="form-control w-full my-2">
+            <div className="label">
+              <span className="label-text">Course name*</span>
+            </div>
+            <input
+              {...register("name")}
+              type="text"
+              placeholder="Course Name"
+              className="input input-bordered w-full"
+            />
+          </label>
+
+          <div className="flex gap-6">
+            <div className=" w-full my-2">
+              <label className="">
+                <div className="label">
+                  <span className="label-text">Course fee*</span>
+                </div>
+                <input
+                  {...register("fee")}
+                  type="text"
+                  placeholder="Course fee"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+            <div className="w-full my-2">
+              <label className=" ">
+                <div className="label">
+                  <span className="label-text">Course id*</span>
+                </div>
+                <input
+                  {...register("courseId")}
+                  type="text"
+                  placeholder="Course id"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+          </div>
+
+          <div className="flex gap-6">
+            <div className=" w-full my-2">
+              <label className="">
+                <div className="label">
+                  <span className="label-text">Semester*</span>
+                </div>
+                <select
+                  className="select select-primary w-full "
+                  {...register("Semester")}
+                >
+                  <option disabled selected value="1st">
+                    1st
+                  </option>
+                  <option value="3rd">3rd</option>
+                  <option value="5th">5th</option>
+                </select>
+              </label>
+            </div>
+            <div className="w-full my-2">
+              <label className=" ">
+                <div className="label">
+                  <span className="label-text">Department*</span>
+                </div>
+                <input
+                  {...register("department")}
+                  type="text"
+                  placeholder="Department"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+          </div>
+          
+          <div className="flex gap-6">
+            <div className=" w-full my-2">
+              <label className="">
+                <div className="label">
+                  <span className="label-text">Course Instructor*</span>
+                </div>
+                <input
+                  {...register("instructor")}
+                  type="text"
+                  placeholder="Course Instructor"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+            <div className="w-full my-2">
+              <label className=" ">
+                <div className="label">
+                  <span className="label-text">Credit</span>
+                </div>
+                <input
+                  {...register("credit")}
+                  type="text"
+                  placeholder="Credit"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+          </div>
+          <div className="flex gap-6">
+            <div className=" w-full my-2">
+              <label className="">
+                <div className="label">
+                  <span className="label-text">Course Level*</span>
+                </div>
+                <input
+                  {...register("level")}
+                  type="text"
+                  placeholder="Course Level"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+            <div className="w-full my-2">
+              <label className=" ">
+                <div className="label">
+                  <span className="label-text">Campus</span>
+                </div>
+                <input
+                  {...register("campus")}
+                  type="text"
+                  placeholder="Campus"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+          </div>
+
+          <div className="flex gap-6">
+            <div className=" w-full my-2">
+              <label className="">
+                <div className="label">
+                  <span className="label-text">Method*</span>
+                </div>
+                <select
+                  className="select select-primary w-full "
+                  {...register("method")}
+                >
+                  <option disabled selected value="1st">
+                    Online
+                  </option>
+                  <option value="3rd">Offline</option>
+                  <option value="5th">Online & Offline both</option>
+                </select>
+              </label>
+            </div>
+            <div className="w-full my-2">
+              <label className=" ">
+                <div className="label">
+                  <span className="label-text">Class Time*</span>
+                </div>
+                <input
+                  {...register("time")}
+                  type="text"
+                  placeholder="Class Time"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+          </div>
+          <div className="flex gap-6">
+            <div className=" w-full my-2">
+              <label className="">
+                <div className="label">
+                  <span className="label-text">Date Range*</span>
+                </div>
+                <input
+                  {...register("dateRange")}
+                  type="text"
+                  placeholder="Date Range"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+            <div className="w-full my-2">
+              <label className=" ">
+                <div className="label">
+                  <span className="label-text">About Course description*</span>
+                </div>
+                <input
+                  {...register("courseId")}
+                  type="text"
+                  placeholder="Course id"
+                  className="input input-bordered w-full"
+                />
+              </label>
+            </div>
+          </div>
+
           <input type="submit" />
         </form>
       </div>
@@ -27,26 +212,13 @@ const AddCourse = () => {
 
 export default AddCourse;
 
-// id
-// "ACC401"
-// fee
-// 200
-// name
-// "Introduction to Managerial Accounting"
-// Department
-// "Business Adminstration"
-// Campus
-// "KU2 Hill"
-// Level
-// "Graduate"
-// Instructor
-// "ohn Hagensy (PhD)"
-// Semester
-// "Fall 2018"
-// Credit
-// "3.000"
-// Method
-// "Lecture"
+
+
+
+
+
+
+
 // description
 // "If you’re an educational professional who are looking to progress into…"
 // description2
@@ -55,12 +227,7 @@ export default AddCourse;
 // "The MSc Finance (EG. Banking) deepens your understanding of banks and …"
 // description4
 // "The MSc Finance (EG. Banking) deepens your understanding of banks and …"
-// time
-// "9:00am – 11:00am"
-// room
-// "122"
-// dateRange
-// "Aug 21, 2018 – Dec 15, 2018"
+
 // img1
 // "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQCqUdeq3DMB_nVFh…"
 // img2
