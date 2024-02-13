f744df1fe3d679ee1765cb68230ade628bcbd842
saikat <tawhidulislam3482@gmail.com>
2024-02-13 22:01:23 +0600

    error fix

--- src/Components/Dashboard/MYCart/MyCart.jsx
+++ src/Components/Dashboard/MYCart/MyCart.jsx
@@ -10,7 +10,7 @@ const MyCart = () => {
   
   const handleDelete = (id) => {
     Swal.fire({
-      title: "Are you sure?",
+      title: "Are you sure to delete it?",
       text: "You won't be able to revert this!",
       icon: "warning",
       showCancelButton: true,
